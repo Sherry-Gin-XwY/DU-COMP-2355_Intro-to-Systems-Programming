abb51885e2eed350838468ed7bcde5da1408db4e
Sherry_Gin <XuWeiyeSherry@gmail.com>
2019-04-06 19:31:13 -0600

    working on the lab 2

---
+++ Lab 02/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+    
+    //1.
+    std::cout << "The size of bool is : " << sizeof(bool) << " bytes \n";
+    
+    std::cout << "The size of char is : " << sizeof(char) << " bytes \n";
+
+    std::cout << "The size of short is : " << sizeof(short) << " bytes \n";
+
+    std::cout << "The size of int is : " << sizeof(int) << " bytes \n";
+
+    std::cout << "The size of long is " << sizeof(long) << " bytes \n";
+
+    std:: cout << "The size of long long is " << sizeof(long long) << " bytes \n";
+
+    std::cout << "The size of float is " << sizeof(float) << " bytes \n";
+
+    std::cout << "The size of double is " << sizeof(double) << " bytes \n";
+
+    std::cout << "The size of long double is " << sizeof(long double) << " bytes \n";
+
+    ////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
+
+
+    //2.
+}
